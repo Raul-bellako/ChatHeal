c5378abd19eacc9300c210101e577fb7ee1cd8ba
Raul-bellako <125909103+Raul-bellako@users.noreply.github.com>
2023-10-24 20:29:13 -0600

    Se agrego el proyecto base de laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main_layout')
+   @section('content') 
+    <section id="about">
+        <h2>Acerca de Chatheal</h2>
+        <p>Chatheal es un chatbot basado en inteligencia artificial que brinda apoyo emocional y consejos de salud mental a través de dispositivos móviles. Nuestra misión es promover la salud mental y proporcionar una comunidad de ayuda mutua en línea.</p>
+    </section>
+    
+    <section id="features">
+        <h2>Características Clave</h2>
+        <ul>
+            <li>Conversaciones de apoyo emocional las 24 horas del día.</li>
+            <li>Consejos personalizados basados en tu estado emocional.</li>
+            <li>Juegos educativos para la gestión de la salud mental.</li>
+            <li>Conexión con voluntarios capacitados en escucha activa.</li>
+        </ul>
+    </section>
+    
+    <section id="get-started">
+        <h2>¡Comienza tu Viaje hacia el Bienestar Emocional!</h2>
+        <a href="registro.html">Regístrate Ahora</a>
+    </section>
+    
+   
---
+++ resources/views/layouts/main_layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chatheal - Tu Compañero en Salud Mental</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="about.html">Acerca de</a></li>
+                <li><a href="features.html">Características</a></li>
+                <li><a href="contact.html">Contacto</a></li>
+            </ul>
+        </nav>
+        <h1>Bienvenido a Chatheal</h1>
+        <p>Tu Compañero Digital para el Bienestar Emocional</p>
+    </header>
+
+    @yield('content')
+
+    <footer>
+        <p>&copy; 2023 Chatheal - Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
